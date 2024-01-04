f6fccf336a95dda9faa2eea385263c9cfeb3d19d
Arjun101105 <91782844+Arjun101105@users.noreply.github.com>
2024-01-04 08:07:32 +0000

    datatype summary

---
+++ 01_basics/comparision.js
@@ -0,0 +1,17 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+// === => this is a strict check .... 
+
+// ********** Datatypes Summary *********** //
+// Primitive 
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+// symbol used to make a value UNIQUE
+
+
+// Javascript is a dynamically typed language. data type will be assigned automatically at the time of compilation/code execution
+
+
+// Reference Type (non primitive)
+
+// Array, Objects, Functions
