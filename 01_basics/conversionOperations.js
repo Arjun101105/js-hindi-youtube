a898c9f87a47a38035578b06414e19da4dcbd7ae
Arjun101105 <91782844+Arjun101105@users.noreply.github.com>
2024-01-04 07:36:31 +0000

    conversion operations in datatypes

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -32,3 +32,26 @@ let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
 
+// *************** Operations ***************//
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2**2) => 2 to the power to , similarly 2**3 => 2 to the power 3//
+
+let str1 = "hello"
+let str2 = " Arjun"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(2 + 2 + "1")
+// this type of code is very messsy and unreadable.... make use of parenthesis
+
+console.log(+true); // + true => 1 .... and empty parenthesis +"" => 0
+console.log(+"")
+
+// prefix and postfix in js mdn
+
